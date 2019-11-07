6eff16855aad9ebadc9ec94e5ac423f9676cafab
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-07 13:33:55 +0100

    Avance en el trabajo, vamos a por el punto 5

--- practica1/aima-java-aima3e-v1.8.1.zip_expanded/aima-java-aima3e-v1.8.1/aima-gui/src/main/java/aima/gui/demo/search/NQueensDemo2.java
+++ practica1/aima-java-aima3e-v1.8.1.zip_expanded/aima-java-aima3e-v1.8.1/aima-gui/src/main/java/aima/gui/demo/search/NQueensDemo2.java
@@ -1,6 +1,7 @@
 package aima.gui.demo.search;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -22,7 +23,9 @@ import aima.core.search.framework.SearchAgent;
 import aima.core.search.framework.TreeSearch;
 import aima.core.search.local.GeneticAlgorithm;
 import aima.core.search.local.HillClimbingSearch;
+import aima.core.search.local.HillClimbingSearch.SearchOutcome;
 import aima.core.search.local.Individual;
+import aima.core.search.local.Scheduler;
 import aima.core.search.local.SimulatedAnnealingSearch;
 import aima.core.search.uninformed.BreadthFirstSearch;
 import aima.core.search.uninformed.DepthFirstSearch;
@@ -45,17 +48,17 @@ public class NQueensDemo2 {
 	}
 
 	private static void newNQueensDemo() {
-//		nQueensWithDepthFirstSearch();
-//		nQueensWithBreadthFirstSearch();
-//		nQueensWithRecursiveDLS();
-//		nQueensWithIterativeDeepeningSearch();
-//		nQueensSimulatedAnnealingSearch();
-//		nQueensHillClimbingSearch();
-//		nQueensGeneticAlgorithmSearch();
-		nQueensHillClimbingSearch_Statistics(10000);
+//		nQueensHillClimbingSearch_Statistics(10000);
+//		System.out.println("-------------------------");
+//		nQueensRandomReestartHillClimbing();
+//		System.out.println("-------------------------");
+//		nQueensSimulatedAnnealingSearch_Statistics(1000);
+//		System.out.println("-------------------------");
+		nQueensHillSimulatedAnnealingRestart();
+		System.out.println("-------------------------");
 	}
 
-	private static void nQueensHillClimbingSearch_Statistics(int numExperiments) {
+	public static void nQueensHillClimbingSearch_Statistics(int numExperiments) {
 		System.out.println("NQueens HillClimbing con " + numExperiments + " estados iniciales diferentes");
 		// Generar numExperiments tableros diferentes
 		List<NQueensBoard> boards = generarTableros(numExperiments);
@@ -76,11 +79,15 @@ public class NQueensDemo2 {
 				switch (search.getOutcome()) {
 				case SOLUTION_FOUND:
 					aciertos += 1;
-					cost_acierto += Float.parseFloat(sa.getInstrumentation().getProperty("pathCost"));
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_acierto += (float) (sa.getActions().size());
 					break;
 				case FAILURE:
 					fallos += 1;
-					cost_fallo += Float.parseFloat(sa.getInstrumentation().getProperty("pathCost"));
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_fallo += (float) (sa.getActions().size());
 					break;
 				}
 			}
@@ -93,130 +100,223 @@ public class NQueensDemo2 {
 		fallos /= numExperiments;
 
 		// Mostramos el resultado de la ejecución
-		System.out.format("Fallos: %s.4\n", fallos);
-		System.out.format("Coste medio fallos: %s.4\n", cost_fallo);
-		System.out.format("Exitos: %s.4\n", aciertos);
-		System.out.format("Coste medio Exitos: %s.4\n", cost_acierto);
+		System.out.format("Fallos: %4.4s\n", fallos);
+		System.out.format("Coste medio fallos: %4.4s\n", cost_fallo);
+		System.out.format("Exitos: %4.4s\n", aciertos);
+		System.out.format("Coste medio Exitos: %4.4s\n", cost_acierto);
+	}
+
+	public static void nQueensRandomReestartHillClimbing() {
+
+		boolean finding = true;
+
+		int intentos = 0, coste_acierto = 0, fallos = 0, coste_fallo = 0;
+
+		HillClimbingSearch search = new HillClimbingSearch(new AttackingPairsHeuristic());
+		NQueensBoard b = null;
+		do {
+			b = new NQueensBoard(8);
+			b.setBoard(generateBoard(8));
+			Problem p = new Problem(b, NQueensFunctionFactory.getCActionsFunction(),
+					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
+			SearchAgent sa = null;
+			try {
+				sa = new SearchAgent(p, search);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (sa != null) {
+				intentos++;
+				switch (search.getOutcome()) {
+				case FAILURE:
+					fallos++;
+					coste_fallo += sa.getActions().size();
+					break;
+				case SOLUTION_FOUND:
+					finding = false;
+					coste_acierto += sa.getActions().size();
+					break;
+				}
+			}
+		} while (finding);
+		// Mostramos los resultados
+		System.out.println("Seach Outcome=" + search.getOutcome());
+		System.out.println("Final State=");
+		System.out.println(search.getLastSearchState() + "\n");
+		System.out.println("Numero de intentos:" + intentos);
+		System.out.println("Fallos:" + fallos);
+		System.out.println("Coste de exito:" + coste_acierto);
+		System.out.println("Coste medio de exito:" + coste_acierto);
+	}
+
+	public static void nQueensSimulatedAnnealingSearch_Statistics(int numExperiments) {
+		System.out.println("NQueens Simulated Annealing con " + numExperiments + " estados iniciales diferentes");
+		int k = 650, limit = 2000;
+		double lam = 0.05;
+		Scheduler scheduler = new Scheduler(k, lam, limit);
+		System.out.printf("Parametros Scheduler: Scheduler(%d,%f,%d)\n", k, lam, limit);
+		// Generar numExperiments tableros diferentes
+		List<NQueensBoard> boards = generarTableros(numExperiments);
+		float fallos = 0, aciertos = 0, cost_fallo = 0, cost_acierto = 0;
+		for (NQueensBoard b : boards) {
+			Problem p = new Problem(b, NQueensFunctionFactory.getCActionsFunction(),
+					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
+			SimulatedAnnealingSearch search = new SimulatedAnnealingSearch(new AttackingPairsHeuristic(), scheduler);
+			SearchAgent sa = null;
+			try {
+				sa = new SearchAgent(p, search);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (sa != null) {
+				// Comprobamos el estado final
+				switch (search.getOutcome()) {
+				case SOLUTION_FOUND:
+					aciertos += 1;
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_acierto += (float) (sa.getActions().size());
+					break;
+				case FAILURE:
+					fallos += 1;
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_fallo += (float) (sa.getActions().size());
+					break;
+				}
+			}
+		}
+
+		// Calculamos la media
+		cost_acierto /= aciertos;
+		cost_fallo /= fallos;
+
+		aciertos /= numExperiments;
+		fallos /= numExperiments;
+
+		// Mostramos el resultado de la ejecución
+		System.out.format("Fallos: %4.4s\n", fallos);
+		System.out.format("Coste medio fallos: %4.4s\n", cost_fallo);
+		System.out.format("Exitos: %4.4s\n", aciertos);
+		System.out.format("Coste medio Exitos: %4.4s\n", cost_acierto);
+	}
+
+	public static void nQueensHillSimulatedAnnealingRestart() {
+
+		boolean finding = true;
+
+		int intentos = 0, coste_acierto = 0, fallos = 0;
+
+		int k = 650, limit = 2000;
+		double lam = 0.05;
+		Scheduler scheduler = new Scheduler(k, lam, limit);
+		SimulatedAnnealingSearch search = new SimulatedAnnealingSearch(new AttackingPairsHeuristic(), scheduler);
+		NQueensBoard b = null;
+		do {
+			b = new NQueensBoard(8);
+			b.setBoard(generateBoard(8));
+			Problem p = new Problem(b, NQueensFunctionFactory.getCActionsFunction(),
+					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
+			SearchAgent sa = null;
+			try {
+				sa = new SearchAgent(p, search);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (sa != null) {
+				intentos++;
+				switch (search.getOutcome()) {
+				case FAILURE:
+					fallos++;
+					break;
+				case SOLUTION_FOUND:
+					finding = false;
+					coste_acierto += sa.getActions().size();
+					break;
+				}
+			}
+		} while (finding);
+		// Mostramos los resultados
+		System.out.println("Seach Outcome=" + search.getOutcome());
+		System.out.println("Final State=");
+		System.out.println(search.getLastSearchState() + "\n");
+		System.out.println("Numero de intentos:" + intentos);
+		System.out.println("Fallos:" + fallos);
+		System.out.println("Coste de exito:" + coste_acierto);
+	}
+
+	public static void nQueenGeneticAlgorithmSearch() {
+		System.out.println("GeneticAlgorithm");
+		// Generar numExperiments tableros diferentes
+
+		int poblacion = 2;
+		double probab_mutacion = 1.0;
+		System.out.printf("Parametros iniciales:\t Poblacion:%d, Probabilidad mutacion:%f)\n", poblacion,
+				probab_mutacion);
+		List<NQueensBoard> boards = generarTableros(poblacion);
+		float fallos = 0, aciertos = 0, cost_fallo = 0, cost_acierto = 0;
+		for (NQueensBoard b : boards) {
+			Problem p = new Problem(b, NQueensFunctionFactory.getCActionsFunction(),
+					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
+			HillClimbingSearch search = new HillClimbingSearch(new AttackingPairsHeuristic());
+			SearchAgent sa = null;
+			try {
+				sa = new SearchAgent(p, search);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (sa != null) {
+				// Comprobamos el estado final
+				switch (search.getOutcome()) {
+				case SOLUTION_FOUND:
+					aciertos += 1;
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_acierto += (float) (sa.getActions().size());
+					break;
+				case FAILURE:
+					fallos += 1;
+					// Comprobamos el tamaño de la lista de acciones a realizar -> profundidad de la
+					// solucion
+					cost_fallo += (float) (sa.getActions().size());
+					break;
+				}
+			}
+		}
+		System.out.println("Mejor individuo=");
+		System.out.println();
 	}
 
 	private static List<NQueensBoard> generarTableros(int numExperiments) {
-		List<NQueensBoard> lista = new LinkedList<NQueensBoard>();
+		ArrayList<NQueensBoard> lista = new ArrayList<NQueensBoard>();
 		int generados = 0;
 		while (generados < numExperiments) {
-			NQueensBoard board = generateBoard(8);
+			NQueensBoard board = new NQueensBoard(8);
+			board.setBoard(generateBoard(8));
 			if (!lista.contains(board)) {
 				lista.add(board);
 				generados++;
+				// System.out.println("Generado nº " + generados);
+				if ((generados % 1000) == 0) {
+					System.out.println(generados);
+				}
 			}
 		}
 		return lista;
 	}
 
-	private static NQueensBoard generateBoard(int n) {
-		NQueensBoard board = new NQueensBoard(n);
+	private static ArrayList<XYLocation> generateBoard(int n) {
 		// Generamos posiciones aleatorias (una por columna)
-		List<XYLocation> tablero = new LinkedList<XYLocation>();
+		ArrayList<XYLocation> tablero = new ArrayList<XYLocation>();
 		for (int i = 0; i < n; i++) {
-			tablero.add(new XYLocation(i, new Random(System.nanoTime()).nextInt(n)));
+			// tablero.add(new XYLocation(i, new Random(System.nanoTime()).nextInt(n)));
+			tablero.add(new XYLocation(i, ((new Random(System.nanoTime()).nextInt() % n) + n) % n));
 		}
-		board.setBoard(tablero);
-		return board;
-	}
-
-	private static void nQueensWithRecursiveDLS() {
-		System.out.println("\nNQueensDemo recursive DLS -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			Search search = new DepthLimitedSearch(_boardSize);
-			SearchAgent agent = new SearchAgent(problem, search);
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private static void nQueensWithBreadthFirstSearch() {
-		try {
-			System.out.println("\nNQueensDemo BFS -->");
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			Search search = new BreadthFirstSearch(new TreeSearch());
-			SearchAgent agent2 = new SearchAgent(problem, search);
-			printActions(agent2.getActions());
-			printInstrumentation(agent2.getInstrumentation());
-		} catch (Exception e1) {
-
-			e1.printStackTrace();
-		}
-	}
-
-	private static void nQueensWithDepthFirstSearch() {
-		System.out.println("\nNQueensDemo DFS -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			Search search = new DepthFirstSearch(new GraphSearch());
-			SearchAgent agent = new SearchAgent(problem, search);
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensWithIterativeDeepeningSearch() {
-		System.out.println("\nNQueensDemo Iterative DS  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			Search search = new IterativeDeepeningSearch();
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensSimulatedAnnealingSearch() {
-		System.out.println("\nNQueensDemo Simulated Annealing  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			SimulatedAnnealingSearch search = new SimulatedAnnealingSearch(new AttackingPairsHeuristic());
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			System.out.println("Search Outcome=" + search.getOutcome());
-			System.out.println("Final State=\n" + search.getLastSearchState());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensHillClimbingSearch() {
-		System.out.println("\nNQueensDemo HillClimbing  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(_boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			HillClimbingSearch search = new HillClimbingSearch(new AttackingPairsHeuristic());
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			System.out.println("Search Outcome=" + search.getOutcome());
-			System.out.println("Final State=\n" + search.getLastSearchState());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		return tablero;
 	}
 
 	public static void nQueensGeneticAlgorithmSearch() {
